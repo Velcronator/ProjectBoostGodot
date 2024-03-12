5dcbe18372cfa270dbeb6eb89f29094cc3ecd3e4
velcronator <valacos7@gmail.com>
2024-03-12 13:08:44 +1100

    Blocking out the level

--- Player.gd
+++ Player.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
 		position.y += delta
